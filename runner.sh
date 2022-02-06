477bdb2685b057262722061807551ce4e3f4126b
Comrade-Remigio <rafael.lixa01@gmail.com>
2022-02-06 11:55:54 +0000

    :ambulance:

--- runner.sh
+++ runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in {3..10000000..10000}
+for i in {5310003..10000000..100000}
 do
     for n in {999990..999999}
     do
         ./remigio $n $i >> leaf_nodes.txt 
     done
-done        
+done
